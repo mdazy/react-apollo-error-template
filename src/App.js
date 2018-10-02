06a607a56befa374c757bab5634e9aeeb06adfc3
dazy <matthieu.dazy@pdgm.com>
2018-10-02 13:12:30 +0200

    Adapt code to match Apollo/TS example, without TS

--- src/App.js
+++ src/App.js
@@ -2,47 +2,27 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 
-class App extends Component {
-  render() {
-    const { data: { loading, people } } = this.props;
-    return (
-      <main>
-        <header>
-          <h1>Apollo Client Error Template</h1>
-          <p>
-            This is a template that you can use to demonstrate an error in
-            Apollo Client. Edit the source code and watch your browser window
-            reload with the changes.
-          </p>
-          <p>
-            The code which renders this component lives in{" "}
-            <code>./src/App.js</code>.
-          </p>
-          <p>
-            The GraphQL schema is in <code>./src/graphql/schema</code>.
-            Currently the schema just serves a list of people with names and
-            ids.
-          </p>
-        </header>
-        {loading ? (
-          <p>Loading…</p>
-        ) : (
-          <ul>
-            {people.map(person => <li key={person.id}>{person.name}</li>)}
-          </ul>
-        )}
-      </main>
-    );
-  }
-}
-
-export default graphql(
-  gql`
-    query ErrorTemplate {
-      people {
-        id
-        name
-      }
+const PEOPLE_QUERY = gql`
+  query ErrorTemplate {
+    people {
+      id
+      name
     }
-  `
-)(App);
+  }
+`;
+
+const withPeople = graphql( 
+  PEOPLE_QUERY, {}
+);
+
+export default withPeople( ( { data: { loading, people, error } } ) => {
+  if( loading ) {
+    return <p>Loading...</p>;
+  }
+  if( error ) {
+    return <h1>Error!</h1>;
+  }
+  return <ul>
+    {people.map( p => <li>{p.id} - {p.name}</li>)}
+  </ul>;
+} );
